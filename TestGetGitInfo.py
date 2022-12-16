4cc2ef5b16c5ee29557fe8fa9519c440c56c71f7
larryyan <yyan27@dons.usfca.edu>
2022-12-16 13:26:22 -0500

    test

--- TestGetGitInfo.py
+++ TestGetGitInfo.py
@@ -4,7 +4,7 @@ from GetGitInfo import get_git_info
 
 class TestGetGitInfo(unittest.TestCase):
     def test_valid_user(self):
-        expected_output = ['User: yyan27',
+        expected_output = ['User: yyan27', 
                     'Repo: GitHubApi567 Number of commits: 5',
                     'Repo: Hello_world Number of commits: 3',
                     'Repo: inclass Number of commits: 23',
